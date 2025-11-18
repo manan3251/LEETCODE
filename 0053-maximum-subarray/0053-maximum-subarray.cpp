e2766445fb6c4d38590877152980e9ee0cb198d8
Manan Arora <167679780+manan3251@users.noreply.github.com>
2025-11-18 21:50:12 +0530

    0053-maximum-subarray

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int start = -1;
-        int n = nums.size();
-        int maxi = INT_MIN;
-        int sum = 0;
+        int start = 0;
         int arr_start = -1;
         int arr_end = -1;
+        int sum = 0;
+        int maxi = INT_MIN;
+        int n = nums.size();
 
         for ( int i = 0 ; i < n ; i++ ) {
-            sum+= nums[i];
-            if ( sum == 0 ) start = i;
+            sum += nums[i];
+            start = i;
 
-            if ( sum > maxi ) {
-                maxi = sum;
-                arr_start = start;
-                arr_end = i; 
-            }
-            if ( sum < 0 ) {
-                sum = 0;
-            }
+        if ( sum > maxi ) {
+            maxi = sum;
+            arr_start = start;
+            arr_end = i;
+        }
+
+        if ( sum < 0 ) sum = 0;
         }
         return maxi;
     }
