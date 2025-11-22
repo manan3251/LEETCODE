313f99770669bb59d41a044b41ed14257ed1e4b5
Manan Arora <167679780+manan3251@users.noreply.github.com>
2025-11-22 16:54:39 +0530

    0033-search-in-rotated-sorted-array

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+ int BS ( vector<int>& nums , int n , int target ) {
+    int low = 0 , high = n-1;
+
+    while ( low <= high ) {
+        int mid = low + ( high - low )/2;
+    
+    if ( nums[mid] == target ) return mid;
+
+    else if (nums[low] <= nums[mid] ) {
+        if ( nums[low] <= target && target <= nums[mid] ) high = mid - 1;
+
+        else low = mid + 1;
+    }
+
+    else {
+        if ( nums[mid] <= target && target <= nums[high] ) low = mid + 1;
+
+        else high = mid - 1;
+    }
+    }
+    return -1;
+ }
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        return BS ( nums , n , target );
+    }
+};
