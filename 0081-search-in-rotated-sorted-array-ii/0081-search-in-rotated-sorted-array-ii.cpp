010ecb83dedb3bab19a669080623fff2e27ba43f
Manan Arora <167679780+manan3251@users.noreply.github.com>
2025-11-22 17:17:07 +0530

    0081-search-in-rotated-sorted-array-ii

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+int BS_Duplicate ( vector <int> nums , int n , int target ) {
+    int low = 0;
+    int high = n-1;
+
+    while ( low <= high ) {
+        int mid = low + (high-low)/2;
+
+    if ( nums[mid] == target ) return mid;
+
+    else if ( nums[low] == nums[mid] && nums[mid] == nums[high] ) {
+        low++;
+        high--;
+        continue;
+    }
+
+    if ( nums[low] <= nums[mid] ) {
+        if ( nums[low] <= target && target <= nums[mid] ) high = mid - 1;
+        else low = mid + 1;
+    }
+
+    else {
+        if ( nums[mid] <= target && target <= nums[high]) low = mid + 1;
+        else high = mid - 1;
+    }
+    }
+    return false;
+}
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        return BS_Duplicate ( nums , n , target );
+    }
+};
