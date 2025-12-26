24968b6460b5ed434762dae40a676e257d303de7
Manan Arora <167679780+manan3251@users.noreply.github.com>
2025-12-26 13:59:26 +0530

    0020-valid-parentheses

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     bool isValid(string s) {
-         stack <char> st;
+        stack <char> st;
 
-         for ( char ch : s ) { 
-            if ( ch == '(' || ch == '{' || ch == '[' ) { 
+        for ( char ch : s ) {
+            if ( ch == '(' || ch == '{' || ch == '[' ) {
                 st.push(ch);
             }
-            else { 
-                if ( st.empty() ) 
-                    return false;
+            else {
+                if ( st.empty() ) return false;
                 char top = st.top();
                 st.pop();
                 if ( ch == ')' && top != '(' ) return false;
                 if ( ch == '}' && top != '{' ) return false;
-                if ( ch == ']' && top != '[' ) return false; 
+                if ( ch == ']' && top != '[' ) return false;
             }
-         }
-         return st.empty();
+        }
+        return st.empty();
     }
 };
