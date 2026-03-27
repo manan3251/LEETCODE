27fdfae1f7d20b2ebe89f665597c63f93db29ac6
Manan Arora <167679780+manan3251@users.noreply.github.com>
2026-03-27 13:50:23 +0530

    0496-next-greater-element-i

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map <int,int> nge;
+        unordered_map<int,int> nge;
         int n = nums2.size();
-        stack <int> st;
+        stack<int> st;
 
-        for ( int i = n-1 ; i >= 0 ; i-- ) {
-            while ( !st.empty() && st.top() < nums2[i] ) {
+        for ( int i = n-1; i >= 0; i-- ) {
+            while (!st.empty() && st.top() < nums2[i] ) {
                 st.pop();
             }
-            if ( st.empty() ) {
+            if ( st.empty())
+            {
                 nge[nums2[i]] = -1;
             }
             else {
                 nge[nums2[i]] = st.top();
             }
+
             st.push(nums2[i]);
-            }
+        }
+        
         vector <int> result;
-        for ( int num : nums1 ) {
+        for ( int num:nums1) {
             result.push_back(nge[num]);
         }
-        return result;
+        return result;    
     }
 };
