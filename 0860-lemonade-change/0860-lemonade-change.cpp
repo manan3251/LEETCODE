f9745aea6be0e5fb0cd537b6e79d7eabcc2c80d6
Manan Arora <167679780+manan3251@users.noreply.github.com>
2026-04-15 02:08:24 +0530

    0860-lemonade-change

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (int i = 0; i < bills.size(); i++) {
+            
+            if (bills[i] == 5) {
+                five++;
+            }
+            
+            else if (bills[i] == 10) {
+                if (five >= 1) {
+                    five--;
+                    ten++;
+                } else {
+                    return false;
+                }
+            }
+            
+            else { // bill == 20
+                if (ten >= 1 && five >= 1) {
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3) {
+                    five -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
