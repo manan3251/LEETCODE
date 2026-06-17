7a0419a554468d267616cea78d9c45fb34a1cdb1
Manan Arora <167679780+manan3251@users.noreply.github.com>
2026-06-17 14:45:15 +0530

    0136-single-number

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xor1 = 0;
         int n = nums.size();
+
+        int xor1 = 0;
+
         for ( int i = 0 ; i < n ; i++ ) {
             xor1 = xor1 ^ nums[i];
         }
         return xor1;
+        
     }
 };
