5eddb1e7ee2e45b06d6b1db82cc57d33aba18e7f
Manan Arora <167679780+manan3251@users.noreply.github.com>
2026-06-25 17:20:34 +0530

    0048-rotate-image

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
 
-        // Step 1: transpose
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                swap(matrix[i][j], matrix[j][i]);
-            }
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        for ( int i = 0 ; i < row ; i++ ) {
+            for ( int j = i+1 ; j < col ; j++ ) {
+                swap (matrix[i][j] , matrix[j][i] );
+            } 
         }
 
-        // Step 2: reverse each row
-        for(int i = 0; i < n; i++){
-            reverse(matrix[i].begin(), matrix[i].end());
+        for ( int k = 0 ; k < row ; k++ ) {
+            reverse ( matrix[k].begin() , matrix[k].end() );
         }
     }
 };
