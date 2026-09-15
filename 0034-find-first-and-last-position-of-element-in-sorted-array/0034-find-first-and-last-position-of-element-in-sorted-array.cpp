27f2353eab1a89d36975856297180f3614ca2a18
Manan Arora <167679780+manan3251@users.noreply.github.com>
2026-09-15 15:28:42 +0530

    0034-find-first-and-last-position-of-element-in-sorted-array

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,47 +1,55 @@
 class Solution {
 public:
-    int FirstOcc(vector<int>& arr, int size, int key) {
-        int s = 0;        
-        int e = size - 1; 
-        int ans = -1;
+int FirstOcc ( vector<int>& arr, int size, int key) { 
+    int s = 0;
+    int n = size;
+    int e = size-1;
+    int ans = -1;
 
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (arr[mid] == key) {
-                ans = mid;
-                e = mid - 1;
-            } else if (key > arr[mid]) {
-                s = mid + 1;
-            } else {
-                e = mid - 1;
-            }
-        }
-        return ans;
+    while ( s <= e ) {
+        int mid = s + ( e - s )/2;
+
+    if ( key == arr[mid] ) {
+        e = mid - 1;
+        ans = mid;
+    } 
+    else if ( key > arr[mid] ) {
+        s = mid + 1;
     }
-
-    int LastOcc(vector<int>& arr, int size, int key) {
-        int s = 0;        
-        int e = size - 1; 
-        int ans = -1;
-
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (arr[mid] == key) {
-                ans = mid;
-                s = mid + 1; 
-            } else if (key > arr[mid]) {
-                s = mid + 1;
-            } else {
-                e = mid - 1;
-            }
-        }
-        return ans;
+    else {
+        e = mid - 1;
     }
+    }
+    return ans;
+}
+
+int LastOcc ( vector<int>& arr, int size, int key) { 
+    int s = 0;
+    int n = size;
+    int e = size-1;
+    int ans = -1;
+
+    while ( s <= e ) {
+        int mid = s + ( e - s )/2;
+
+    if ( key == arr[mid] ) {
+        s = mid + 1;
+        ans = mid;
+    } 
+    else if ( key > arr[mid] ) {
+        s = mid + 1;
+    }
+    else {
+        e = mid - 1;
+    }
+    }
+    return ans;
+}
 
     vector<int> searchRange(vector<int>& nums, int target) {
-        int size = nums.size();
-        int first = FirstOcc(nums, size, target);
-        int last = LastOcc(nums, size, target);
-        return {first, last};
-    }
+        int n = nums.size();
+        int first = FirstOcc( nums, n, target);
+        int last = LastOcc ( nums, n, target);
+        return { first, last};
+    }  
 };
