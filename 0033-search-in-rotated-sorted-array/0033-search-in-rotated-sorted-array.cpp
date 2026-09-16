e3ba732201b6186ef382ff9f0af3c8f3a68d908e
Manan Arora <167679780+manan3251@users.noreply.github.com>
2026-09-16 12:38:57 +0530

    0033-search-in-rotated-sorted-array

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
- int BS ( vector<int>& nums , int n , int target ) {
-    int low = 0 , high = n-1;
+
+int BS ( vector<int>& nums, int n, int x ) {
+    int low = 0;
+    int high = n -1;
 
     while ( low <= high ) {
-        int mid = low + ( high - low )/2;
-    
-    if ( nums[mid] == target ) return mid;
+        int mid = low + ( ( high - low ) / 2 );
 
-    else if (nums[low] <= nums[mid] ) {
-        if ( nums[low] <= target && target <= nums[mid] ) high = mid - 1;
+    if ( x == nums[mid] ) return mid;
 
-        else low = mid + 1;
+    else if ( nums[low] <= nums[mid] ) {
+        if ( nums[low] <= x && x <= nums[mid] ) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
     }
 
     else {
-        if ( nums[mid] <= target && target <= nums[high] ) low = mid + 1;
-
-        else high = mid - 1;
+        if ( nums[mid] <= x && x <= nums[high] ) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
     }
-    }
+}
     return -1;
- }
+    }
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-        return BS ( nums , n , target );
+        return BS ( nums, n, target );
+        
     }
 };
