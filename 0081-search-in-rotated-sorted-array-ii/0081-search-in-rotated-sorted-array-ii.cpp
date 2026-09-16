e669d5b44cd49f60ea6c4004a4a8b219599f6a17
Manan Arora <167679780+manan3251@users.noreply.github.com>
2026-09-16 15:48:17 +0530

    0081-search-in-rotated-sorted-array-ii

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
-bool BS_Duplicate ( vector <int> nums , int n , int target ) {
-    int low = 0;
-    int high = n-1;
 
-    while ( low <= high ) {
-        int mid = low + (high-low)/2;
+int BS ( vector<int>& nums, int n, int x ) {
+    int start = 0;
+    int end = n-1;
 
-    if ( nums[mid] == target ) return true;
+    while ( start <= end ) {
+        int mid = start + ((end -start)/2);
 
-    else if ( nums[low] == nums[mid] && nums[mid] == nums[high] ) {
-        low++;
-        high--;
+    if ( x == nums[mid] ) return true;
+    if ( nums[start] == nums[mid] && nums[mid] == nums[end] ) {
+        start = start + 1;
+        end = end - 1;
         continue;
     }
 
-    if ( nums[low] <= nums[mid] ) {
-        if ( nums[low] <= target && target <= nums[mid] ) high = mid - 1;
-        else low = mid + 1;
-    }
-
+    else if ( nums[start] <= nums[mid] ) {
+        if (  nums[start] <= x && x <= nums[mid] ) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+    }  
+    
     else {
-        if ( nums[mid] <= target && target <= nums[high]) low = mid + 1;
-        else high = mid - 1;
+        if ( nums[mid] <= x && x <= nums[end] ) {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
     }
     }
     return false;
 }
+
     bool search(vector<int>& nums, int target) {
         int n = nums.size();
 
-        return BS_Duplicate ( nums , n , target );
+        return BS ( nums, n, target);
+        
     }
 };
