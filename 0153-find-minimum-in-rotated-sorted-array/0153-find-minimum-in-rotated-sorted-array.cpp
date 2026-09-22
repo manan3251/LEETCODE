5ad111652b76ba00f813348de34901a8afedc6a9
Manan Arora <167679780+manan3251@users.noreply.github.com>
2026-09-22 17:59:28 +0530

    0153-find-minimum-in-rotated-sorted-array

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         int n = nums.size();
-        int low = 0 , high = n - 1 , ans = INT_MAX;
+        int start = 0 , end = n - 1 , ans = INT_MAX;
 
-        while ( low <= high ) {
-            int mid = low + (high - low)/2;
-            if ( nums[low] <= nums[mid] ) {
-                ans = min(ans , nums[low]);
-                low = mid+1;
-            }
-            else {
-                ans = min(ans , nums[mid]);
-                high = mid - 1;
-            }
-        }
+    while ( start <= end ) {
+        int mid = start + ( end - start )/2;
+
+    if ( nums[start] <= nums[mid] ) {
+        ans = min(ans, nums[start]);
+        start = mid + 1;
+    }
+    else {
+        ans = min(ans, nums[mid]);
+        end = mid - 1;
+    }
+    }
         return ans;
     }
 };
