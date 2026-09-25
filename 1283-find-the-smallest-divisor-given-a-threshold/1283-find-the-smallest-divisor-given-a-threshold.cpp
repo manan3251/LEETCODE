470014771003943c0e7be19e42adda2bc2384caf
Manan Arora <167679780+manan3251@users.noreply.github.com>
2026-09-25 14:17:23 +0530

    1283-find-the-smallest-divisor-given-a-threshold

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
+int sumofD ( vector<int>& arr , int div ) {
+    int sum = 0;
+    int n = arr.size();
 
-long long smallest_divisor ( vector<int>& nums , int div ) {
-    long long sum = 0;
-    int n = nums.size();
     for ( int i = 0 ; i < n ; i++ ) {
-        sum = sum + ceil ( (double)(nums[i]) / (double)(div));
+        sum += ceil ( (double)(arr[i]) / (double)(div) ); 
     }
     return sum;
-}
+} 
+
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int n = nums.size();
-        int low = 1;
-        int high = *max_element(nums.begin() , nums.end() );
+        int start = 1;
+        int end = *max_element(nums.begin() , nums.end() );
+        int ans = -1;
 
-        while ( low <= high ) {
-            int mid = low + ( high - low )/2;
+        while ( start <= end ) {
+            int mid = start + ((end - start) / 2);
 
-        if ( smallest_divisor( nums , mid ) <= threshold ) {
-            high = mid - 1;
-        } 
-        else low = mid + 1;
+        if ( sumofD( nums , mid ) <= threshold ) {
+            ans = mid;
+            end = mid - 1;
         }
-        return low;
+        else {
+            start = mid + 1;
+        }
+        }
+        return start;
     }
 };
